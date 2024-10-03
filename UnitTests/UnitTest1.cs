f5b83eb7e44ba428721254979a2969afed3f45ab
Олег Чижов <oleh.chyzhov@lnu.edu.ua>
2024-10-03 11:05:46 +0300

    Налаштував UnitTests

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+//using BusinessLogic; Розкоментувати, коли в проекті BusinessLogic буде хоч 1 клас 
+//using DataAccess; Розкоментувати, коли в проекті DataAccess буде хоч 1 клас 
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
