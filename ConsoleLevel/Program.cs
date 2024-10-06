5d4dc0fe7fb2581a6195bc5a842ee84a8742105e
Юлія Тимочко <yuliia.tymochko@lnu.edu.ua>
2024-10-06 22:31:46 +0300

    Додала дані до таблиці Account та вивела їх

--- ConsoleLevel/Program.cs
+++ ConsoleLevel/Program.cs
@@ -5,21 +5,21 @@ class Program
 
     public static void FillAccountsTable()
     {
-        DbHelper.db.Add(new Account() { Email = "oleh.chyzhov@gmail.com", Password = "Oleg2005" });
-        DbHelper.db.Add(new Account() { Email = "nazar.valaga@gmail.com", Password = "12345" });
-        DbHelper.db.Add(new Account() { Email = "yulia.tymochko@gmail.com", Password = "54321" });
-        DbHelper.db.Add(new Account() { Email = "roman.torskiy@gmail.com", Password = "46532" });
-        DbHelper.db.Add(new Account() { Email = "roman.shmyhelskiy@gmail.com", Password = "13562" });
-        DbHelper.db.Add(new Account() { Email = "anastasiya.seliverstova@gmail.com", Password = "46532" });
-        DbHelper.db.Add(new Account() { Email = "valeriya.ponomariova@gmail.com", Password = "53241" });
-        DbHelper.db.Add(new Account() { Email = "olena.kupchak@gmail.com", Password = "52413" });
-        DbHelper.db.Add(new Account() { Email = "veronika.filippova@gmail.com", Password = "46513" });
-        DbHelper.db.Add(new Account() { Email = "lilya.voloshchuk@gmail.com", Password = "13524" });
-        DbHelper.db.Add(new Account() { Email = "oleh.diduch@gmail.com", Password = "63425" });
-        DbHelper.db.Add(new Account() { Email = "markian.kravets@gmail.com", Password = "13546" });
-        DbHelper.db.Add(new Account() { Email = "oleh.kit@gmail.com", Password = "24531" });
-        DbHelper.db.Add(new Account() { Email = "nazar.midyk@gmail.com", Password = "11111" });
-        DbHelper.db.Add(new Account() { Email = "maks.salo@gmail.com", Password = "55555" });
+        //DbHelper.db.Add(new Account() { Email = "oleh.chyzhov@gmail.com", Password = "Oleg2005" });
+        //DbHelper.db.Add(new Account() { Email = "nazar.valaga@gmail.com", Password = "12345" });
+        //DbHelper.db.Add(new Account() { Email = "yulia.tymochko@gmail.com", Password = "54321" });
+        //DbHelper.db.Add(new Account() { Email = "roman.torskiy@gmail.com", Password = "46532" });
+        //DbHelper.db.Add(new Account() { Email = "roman.shmyhelskiy@gmail.com", Password = "13562" });
+        //DbHelper.db.Add(new Account() { Email = "anastasiya.seliverstova@gmail.com", Password = "46532" });
+        //DbHelper.db.Add(new Account() { Email = "valeriya.ponomariova@gmail.com", Password = "53241" });
+        //DbHelper.db.Add(new Account() { Email = "olena.kupchak@gmail.com", Password = "52413" });
+        //DbHelper.db.Add(new Account() { Email = "veronika.filippova@gmail.com", Password = "46513" });
+        //DbHelper.db.Add(new Account() { Email = "lilya.voloshchuk@gmail.com", Password = "13524" });
+        //DbHelper.db.Add(new Account() { Email = "oleh.diduch@gmail.com", Password = "63425" });
+        //DbHelper.db.Add(new Account() { Email = "markian.kravets@gmail.com", Password = "13546" });
+        //DbHelper.db.Add(new Account() { Email = "oleh.kit@gmail.com", Password = "24531" });
+        //DbHelper.db.Add(new Account() { Email = "nazar.midyk@gmail.com", Password = "11111" });
+        //DbHelper.db.Add(new Account() { Email = "maks.salo@gmail.com", Password = "55555" });
         DbHelper.db.Add(new Account() { Email = "olesia.rudevych@gmail.com", Password = "olesia111" });
         DbHelper.db.Add(new Account() { Email = "pavlo.smahula@gmail.com", Password = "psmhl01" });
         DbHelper.db.Add(new Account() { Email = "yulia.holub@gmail.com", Password = "yuliah22" });
