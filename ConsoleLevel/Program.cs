220568582184286f8567208d6f920284a9580e9a
Назар Валага <nazar.valaha@lnu.edu.ua>
2024-10-06 22:24:04 +0300

    Додав нові дані до функції FillAccountsTable

--- ConsoleLevel/Program.cs
+++ ConsoleLevel/Program.cs
@@ -2,7 +2,7 @@
 using DataAccess.Models;
 class Program
 {
-    
+
     public static void FillAccountsTable()
     {
         DbHelper.db.Add(new Account() { Email = "oleh.chyzhov@gmail.com", Password = "Oleg2005" });
@@ -20,8 +20,24 @@ class Program
         DbHelper.db.Add(new Account() { Email = "oleh.kit@gmail.com", Password = "24531" });
         DbHelper.db.Add(new Account() { Email = "nazar.midyk@gmail.com", Password = "11111" });
         DbHelper.db.Add(new Account() { Email = "maks.salo@gmail.com", Password = "55555" });
+        DbHelper.db.Add(new Account() { Email = "olesia.rudevych@gmail.com", Password = "olesia111" });
+        DbHelper.db.Add(new Account() { Email = "pavlo.smahula@gmail.com", Password = "psmhl01" });
+        DbHelper.db.Add(new Account() { Email = "yulia.holub@gmail.com", Password = "yuliah22" });
+        DbHelper.db.Add(new Account() { Email = "maksym.slipkevych@gmail.com", Password = "maks02" });
+        DbHelper.db.Add(new Account() { Email = "andriy.stefurak@gmail.com", Password = "stefchuk333" });
+        DbHelper.db.Add(new Account() { Email = "maksym.kuzelyak@gmail.com", Password = "maksik03" });
+        DbHelper.db.Add(new Account() { Email = "nastya.sashchack@gmail.com", Password = "nastya444" });
+        DbHelper.db.Add(new Account() { Email = "vika.mochevynska@gmail.com", Password = "vikim04" });
+        DbHelper.db.Add(new Account() { Email = "anna.lukianchuk@gmail.com", Password = "annaluk555" });
+        DbHelper.db.Add(new Account() { Email = "anna.tkach@gmail.com", Password = "anyuta05" });
+        DbHelper.db.Add(new Account() { Email = "uliana.maydanska@gmail.com", Password = "umdnsk666" });
+        DbHelper.db.Add(new Account() { Email = "kristian.matiyishyn@gmail.com", Password = "krism06" });
+        DbHelper.db.Add(new Account() { Email = "misha.chekan@gmail.com", Password = "miha777" });
+        DbHelper.db.Add(new Account() { Email = "yulia.bohatyr@gmail.com", Password = "yuli07" });
+        DbHelper.db.Add(new Account() { Email = "tanya.mazyr@gmail.com", Password = "tanya888" });
         DbHelper.db.SaveChanges();
     }
+
     public static void Main(string[] args)
     {
         //FillAccountsTable();
